e5841246ef8bac22d2b1f85112715f3f8cd13dc9
MaxLyFe <maximilianolyfbuzios@gmail.com>
2022-09-26 20:24:11 -0300

    Basic project reviews

---
+++ app.js
@@ -0,0 +1,38 @@
+const reviews = [
+
+    {
+        id: 1,
+        name: 'Santiago',
+        job: 'Web developer',
+        img: 'https://s3-sa-east-1.amazonaws.com/doctoralia.com.br/doctor/912957/91295778c2a22a7fd7d91496b859f1e0_large.jpg',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec accumsan sem, eget ultrices neque. Cras laoreet eros nisl, ut porttitor eros vulputate non. Mauris finibus, dui eget gravida aliquet, arcu mi fermentum sem, sit amet volutpat ligula neque quis eros. Nullam molestie lectus ipsum.',
+    },
+    {
+        id: 2,
+        name: 'Nikolas',
+        job: 'Garçom',
+        img: 'https://kedimanassif.com.br/wp-content/uploads/2021/01/Como-saber-se-a-pessoa-vai-ficar-careca.jpg',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec accumsan sem, eget ultrices neque. Cras laoreet eros nisl, ut porttitor eros vulputate non. Mauris finibus, dui eget gravida aliquet, arcu mi fermentum sem, sit amet volutpat ligula neque quis eros. Nullam molestie lectus ipsum.',
+    },
+    {
+        id: 3,
+        name: 'Mike',
+        job: 'Developer Jr.',
+        img: 'https://media.infopedia.pt/p/102/sp/10200/thumbnail/entry_id/0_0uo87egy/version/100002/width/380/height/249',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec accumsan sem, eget ultrices neque. Cras laoreet eros nisl, ut porttitor eros vulputate non. Mauris finibus, dui eget gravida aliquet, arcu mi fermentum sem, sit amet volutpat ligula neque quis eros. Nullam molestie lectus ipsum.',
+    },
+    {
+        id: 4,
+        name: 'Kaio',
+        job: 'YouTuber',
+        img: 'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2022/01/272659509_640826040498160_305754906527072885_n-e1643549776803.jpg?w=876&h=484&crop=1',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec accumsan sem, eget ultrices neque. Cras laoreet eros nisl, ut porttitor eros vulputate non. Mauris finibus, dui eget gravida aliquet, arcu mi fermentum sem, sit amet volutpat ligula neque quis eros. Nullam molestie lectus ipsum.',
+    },
+    {
+        id: 5,
+        name: 'Kauan',
+        job: 'Developer Sernior',
+        img: 'https://static.cobrefacil.com.br/assets/base/035/3ab/4cb/gerar-boleto-gratis-pessoa-fisica.jpg',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec accumsan sem, eget ultrices neque. Cras laoreet eros nisl, ut porttitor eros vulputate non. Mauris finibus, dui eget gravida aliquet, arcu mi fermentum sem, sit amet volutpat ligula neque quis eros. Nullam molestie lectus ipsum.',
+    }
+];
